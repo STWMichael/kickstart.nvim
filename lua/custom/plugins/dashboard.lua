e0568f0a3aa16a5e8a314b0a56714b0705b53b06
michael_wang <stw183164761@gmail.com>
2025-07-05 00:05:57 -0400

    changed username

--- lua/custom/plugins/dashboard.lua
+++ lua/custom/plugins/dashboard.lua
@@ -57,7 +57,7 @@ return {
     local greeting = function()
       -- Determine the appropriate greeting based on the hour
       local mesg
-      local username = os.getenv 'USERNAME'
+      local username = 'Michael'
       if datetime >= 0 and datetime < 6 then
         mesg = 'Dreaming..󰒲 󰒲 '
       elseif datetime >= 6 and datetime < 12 then
